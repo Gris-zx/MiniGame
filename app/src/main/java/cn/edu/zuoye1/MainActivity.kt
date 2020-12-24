f8cdacfd14bfbbbd24d53b26d4f21c1066278400
gris-zx <1713787511@qq.com>
2020-12-24 20:17:59 +0800

    second commit.

--- app/src/main/java/cn/edu/zuoye1/MainActivity.kt
+++ app/src/main/java/cn/edu/zuoye1/MainActivity.kt
@@ -2,6 +2,7 @@ package cn.edu.zuoye1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
             textView.text = resources.getString(R.string.clicked)
+            Log.d("sadaa","sadasda")
         }
     }
 }
