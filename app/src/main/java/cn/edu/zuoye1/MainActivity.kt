a842416937afaff1acc422ae1157c7721e793147
gris-zx <1713787511@qq.com>
2020-12-24 20:12:56 +0800

    first commit.

---
+++ app/src/main/java/cn/edu/zuoye1/MainActivity.kt
@@ -0,0 +1,16 @@
+package cn.edu.zuoye1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            textView.text = resources.getString(R.string.clicked)
+        }
+    }
+}
